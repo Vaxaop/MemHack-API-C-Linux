26d82ca9ac3bd06c8c5f2a188039c18586116eac
Vaxaop <115424008+Vaxaop@users.noreply.github.com>
2023-01-13 13:36:08 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "Mem-Api.h"
 
-// command to compile = gcc -std=c17 -m64 -Wall utils.h main.c -o main
-// dont forget to run your programme in sudo ex sudo ./programme-name
+// command to compile = gcc -std=c17 -m64 -Wall Mem-Api.h main.c -o main
+// dont forget to run your programme in sudo ex sudo ./main
 
 int main(int argc, char ** argv) {
     ulongl addr = 0x5565200c0010;
