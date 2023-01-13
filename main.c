bc3d8c0e60ba53edde033f154722aa34abf756fc
Vaxaop <115424008+Vaxaop@users.noreply.github.com>
2023-01-13 13:41:03 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,14 @@ int main(int argc, char ** argv) {
     ulongl addr = 0x5565200c0010; //change the address here
     uint processID = GetTargetPid("TestProgramme");
     printf("The pid of the test programme is : %d\n", processID);
+    
     void * value_read = RPM(addr, sizeof(int));
     printf("The value read = %d\n", *(int*)value_read);
+    
     int write_value = 1234;
     WPM(addr, &write_value , sizeof(write_value ));
     printf("value just got write to it\n");
+    
     free(value_read);
     return 0;
 }
