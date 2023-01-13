37b51ab0d2bd455172b743231be81fed31a1a255
Vaxaop <115424008+Vaxaop@users.noreply.github.com>
2023-01-13 13:37:35 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,14 @@
 // dont forget to run your programme in sudo ex sudo ./main
 
 int main(int argc, char ** argv) {
-    ulongl addr = 0x5565200c0010;
+    ulongl addr = 0x5565200c0010; //change the address here
     uint processID = GetTargetPid("TestProgramme");
     printf("The pid of the test programme is : %d\n", processID);
     void * value_read = RPM(addr, sizeof(int));
     printf("The value read = %d\n", *(int*)value_read);
-    int vriting_valuee = 1234;
-    WPM(addr, &vriting_valuee, sizeof(vriting_valuee));
-    printf("value just got wriiting \n");
+    int write_value = 1234;
+    WPM(addr, &write_value , sizeof(write_value ));
+    printf("value just got write to it\n");
     free(value_read);
     return 0;
 }
